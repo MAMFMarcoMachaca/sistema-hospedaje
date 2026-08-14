3f5073038199f30a239085a386e096d31b9935a3
Marco Antonio <marco.machaca@tecsup.edu.pe>
2026-08-14 12:21:26 -0500

    refactor: mejorar validaciones y separar funciones del sistema

--- RegistroHabitacion/Program.cs
+++ RegistroHabitacion/Program.cs
@@ -29,249 +29,16 @@ while (programaActivo)
     switch(opcionMenu)
     {
         case 1:
-            Console.WriteLine("=== REGISTRO DE HABITACIÓN ===");
-            Console.WriteLine();
-
-            Console.Write("Número de habitación: ");
-
-        bool numeroValido = int.TryParse(
-            Console.ReadLine(),
-            out int numeroHabitacion
-        );
-
-        if (numeroValido && numeroHabitacion > 0)
-        {
-            Console.WriteLine();
-            Console.WriteLine("Seleccione el tipo de habitación:");
-            Console.WriteLine("1. Simple");
-            Console.WriteLine("2. Doble");
-            Console.WriteLine("3. Matrimonial");
-            Console.WriteLine("4. Familiar");
-            Console.WriteLine("5. Triple");
-            Console.Write("Opción: ");
-
-            bool opcionConvertida = int.TryParse(
-                Console.ReadLine(),
-                out int opcionTipo
-            );
-
-            string tipoHabitacion = "";
-            bool tipoValido = true;
-
-            switch (opcionTipo)
-            {
-                case 1:
-                    tipoHabitacion = "Simple";
-                    break;
-
-                case 2:
-                    tipoHabitacion = "Doble";
-                    break;
-
-                case 3:
-                    tipoHabitacion = "Matrimonial";
-                    break;
-
-                case 4:
-                    tipoHabitacion = "Familiar";
-                    break;
-
-                case 5:
-                    tipoHabitacion = "Triple";
-                    break;
-
-                default:
-                    tipoValido = false;
-                    break;
-            }
-
-            if (opcionConvertida && tipoValido)
-            {
-                Console.WriteLine();
-                Console.Write("Precio por noche: S/ ");
-
-                bool precioConvertido = decimal.TryParse(
-                    Console.ReadLine(),
-                    out decimal precioNoche
-                );
-
-                if (precioConvertido && precioNoche > 0)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Seleccione el estado de la habitación:");
-                    Console.WriteLine("1. Disponible");
-                    Console.WriteLine("2. Ocupada");
-                    Console.Write("Opción: ");
-
-                    bool opcionEstadoConvertida = int.TryParse(
-                        Console.ReadLine(),
-                        out int opcionEstado
-                    );
-
-                    string estadoHabitacion ="";
-                    bool estadoValido = true;
-
-                    switch (opcionEstado)
-                    {
-                        case 1:
-                            estadoHabitacion = "Disponible";
-                            break;
-                        case 2:
-                            estadoHabitacion= "Ocupada";
-                            break;
-                        default:
-                            estadoValido = false;
-                            break;            
-                    }
-
-                    if(opcionEstadoConvertida && estadoValido)
-                    {
-
-                    string habitacionRegistrada =
-                        $"Habitación {numeroHabitacion} | " +
-                        $"Tipo: {tipoHabitacion} | " +
-                        $"Precio: S/ {precioNoche:F2} | " +
-                        $"Estado: {estadoHabitacion}";
-
-                        habitaciones.Add(habitacionRegistrada);
-
-                        Console.WriteLine();
-                        Console.WriteLine("=== HABITACIÓN REGISTRADA ===");
-                        Console.WriteLine(habitacionRegistrada);
-                        Console.WriteLine($"Total registrado: {habitaciones.Count}");
-                        
-                    }
-                    else{
-                        Console.WriteLine();
-                        Console.WriteLine(
-                        "Error: seleccione 1 para Disponible o 2 para Ocupada."
-                    );
-                    }
-
-                }
-                else
-                {
-                    Console.WriteLine();
-                    Console.WriteLine(
-                        "Error: ingrese un precio mayor que cero."
-                    );
-                }
-            }
-            else
-            {
-                Console.WriteLine();
-                Console.WriteLine(
-                    "Error: seleccione una opción entre 1 y 5."
-                );
-            }
-        }
-        else
-        {
-            Console.WriteLine();
-            Console.WriteLine(
-                "Error: ingrese un número de habitación mayor que cero."
-            );
-        }
+            RegistrarHabitacion(habitaciones);
             break;
         case 2:
-            Console.WriteLine();
-            Console.WriteLine("=== REGISTRO DE INGRESO ===");
-
-            Console.WriteLine("Concepto del ingreso: ");
-            string conceptoIngreso = Console.ReadLine() ?? "";
-
-            if(string.IsNullOrWhiteSpace(conceptoIngreso))
-            {
-                Console.WriteLine();
-                Console.WriteLine(
-                    "Error: el concepto no puede estar vacío."
-                );
-
-                break;
-
-            }
-
-            Console.Write("Monto del ingreso: S/ ");
-
-            bool montoConvertido = decimal.TryParse(
-                Console.ReadLine(),
-                out decimal montoIngreso
-            );
-
-            if (montoConvertido && montoIngreso > 0)
-            {
-                string ingresoRegistrado = 
-                    $"{conceptoIngreso} | S/ {montoIngreso:F2}";
-
-                ingresos.Add(ingresoRegistrado);
-                totalIngresos += montoIngreso;
-
-                Console.WriteLine();
-                Console.WriteLine("=== INGRESO REGISTRADO ===");
-                Console.WriteLine(ingresoRegistrado);
-                Console.WriteLine(
-                    $"Total de ingresos registrados: {ingresos.Count}"
-                );
-                Console.WriteLine(
-                    $"Monto acumulado: S/ {totalIngresos:F2}"
-                );
-            }
-            else
-            {
-                Console.WriteLine();
-                Console.WriteLine(
-                    "Error: ingrese un monto mayor que cero."
-                );
-            }
-
+            totalIngresos= RegistrarIngreso(ingresos, totalIngresos);
             break;
+        
         case 3:
-            Console.WriteLine();
-            Console.WriteLine("=== RESUMEN BÁSICO ===");
-
-            Console.WriteLine();
-            Console.WriteLine(
-                $"Habitaciones registradas: {habitaciones.Count}"
-            );
-
-            if(habitaciones.Count == 0)
-            {
-                Console.WriteLine(
-                    "No hay habitaciones registradas."
-                );
-            }
-            else
-            {
-                foreach (string habitacion in habitaciones)
-                {
-                    Console.WriteLine($"- {habitacion}");
-                }
-            }
-
-            Console.WriteLine();
-            Console.WriteLine(
-                $"Ingresos registrados: {ingresos.Count}"
-            );
-
-            if (ingresos.Count == 0)
-            {
-                Console.WriteLine(
-                    "No hay ingresos registrados."
-                );
-            }
-            else
-            {
-                foreach(string ingreso in ingresos)
-                {
-                    Console.WriteLine($" -{ingreso}");
-                }
-            }
-
-            Console.WriteLine();
-            Console.WriteLine(
-                $"Total de igresos: S/ {totalIngresos:F2}"
-            );
+            MostrarResumen(habitaciones, ingresos, totalIngresos);
             break;
+            
         case 4:
             programaActivo = false;
 
@@ -281,9 +48,293 @@ while (programaActivo)
         
         default:
             Console.WriteLine();
-            Console.WriteLine("Error: selecione una opción entre 1 y 4.");
+            Console.WriteLine("Error: seleccione una opción entre 1 y 4.");
             break;
     }
 }
 
 
+
+void MostrarResumen(
+    List<string> habitaciones,
+    List<string> ingresos,
+    decimal totalIngresos
+)
+{
+    Console.WriteLine();
+    Console.WriteLine("=== RESUMEN BÁSICO ===");
+
+    Console.WriteLine();
+    Console.WriteLine(
+        $"Habitaciones registradas: {habitaciones.Count}"
+    );
+
+    if(habitaciones.Count == 0)
+    {
+        Console.WriteLine(
+            "No hay habitaciones registradas."
+        );
+    }
+    else{
+        foreach (string habitacion in habitaciones)
+        {
+            Console.WriteLine($"- {habitacion}");
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine(
+        $"Ingresos registrados: {ingresos.Count}"
+    );
+
+    if (ingresos.Count == 0)
+    {
+        Console.WriteLine(
+            "No hay ingresos registrados."
+        );
+    }
+    else
+    {
+        foreach (string ingreso in ingresos)
+        {
+            Console.WriteLine($"- {ingreso}");
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine(
+        $"Total de ingresos: S/ {totalIngresos:F2}"
+    );
+}
+
+decimal RegistrarIngreso(
+    List<string> ingresos,
+    decimal totalIngresos
+)
+{
+    Console.WriteLine();
+    Console.WriteLine("=== REGISTRO DE INGRESO ===");
+
+    Console.Write("Concepto del ingreso: ");
+    string conceptoIngreso = Console.ReadLine() ?? "";
+
+    if(string.IsNullOrWhiteSpace(conceptoIngreso))
+    {
+        Console.WriteLine();
+        Console.WriteLine(
+            "Error: el concepto no puede estar vacío."
+        );
+
+        return totalIngresos;
+    }
+
+    Console.Write("Monto del ingreso: S/ ");
+
+    bool montoConvertido = decimal.TryParse(
+        Console.ReadLine(),
+        out decimal montoIngreso
+    );
+
+    if (montoConvertido && montoIngreso > 0)
+    {
+        string ingresoRegistrado =
+            $"{conceptoIngreso} | S/ {montoIngreso:F2}";
+        
+        ingresos.Add(ingresoRegistrado);
+
+        totalIngresos += montoIngreso;
+
+        Console.WriteLine();
+        Console.WriteLine("=== INGRESO REGISTRADO ===");
+        Console.WriteLine(ingresoRegistrado);
+        Console.WriteLine(
+            $"Total de ingresos registrados: {ingresos.Count}"
+        );
+        Console.WriteLine(
+            $"Monto acumulado: S/ {totalIngresos:F2}"
+        );
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine(
+            "Error: ingrese un monto mayor que cero."
+        );
+    }
+
+    return totalIngresos;
+}
+
+void RegistrarHabitacion(List<string> habitaciones)
+{
+    Console.WriteLine();
+    Console.WriteLine("=== REGISTRO DE HABITACIÓN ===");
+
+    Console.WriteLine();
+    Console.Write("Número de habitación: ");
+
+    bool numeroValido = int.TryParse(
+        Console.ReadLine(),
+        out int numeroHabitacion
+    );
+
+    if (!numeroValido)
+    {
+        Console.WriteLine();
+        Console.WriteLine(
+            "Error: debe ingresar un número de habitación válido."
+        );
+
+        return;
+    }
+
+    if(numeroHabitacion <= 0)
+    {
+        Console.WriteLine();
+        Console.WriteLine(
+            "Error: el número de habitación debe ser mayor que cero. "
+        );
+
+        return;
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Seleccione el tipo de habitación:");
+    Console.WriteLine("1. Simple");
+    Console.WriteLine("2. Doble");
+    Console.WriteLine("3. Matrimonial");
+    Console.WriteLine("4. Familiar");
+    Console.WriteLine("5. Triple");
+    Console.Write("Opción: ");
+
+    bool opcionConvertida = int.TryParse(
+        Console.ReadLine(),
+        out int opcionTipo
+    );
+
+    string tipoHabitacion ="";
+
+    switch (opcionTipo)
+    {
+        case 1:
+            tipoHabitacion = "Simple";
+            break;
+
+        case 2:
+            tipoHabitacion = "Doble";
+            break;
+        
+        case 3:
+            tipoHabitacion = "Matrimonial";
+            break;
+        
+        case 4:
+            tipoHabitacion = "Familiar";
+            break;
+        
+        case 5:
+            tipoHabitacion = "Triple";
+            break;
+        
+        default:
+            Console.WriteLine();
+            Console.WriteLine(
+                "Error: seleccione una opción entre 1 y 5."
+            );
+
+            return;
+    }
+
+    if (!opcionConvertida)
+    {
+        Console.WriteLine();
+        Console.WriteLine(
+            "Error: debe ingresar una opción numérica."
+        );
+
+        return;
+    }
+
+    Console.WriteLine();
+    Console.Write("Precio por noche: S/ ");
+
+    bool precioConvertido = decimal.TryParse(
+        Console.ReadLine(),
+        out decimal precioNoche
+    );
+
+    if(!precioConvertido)
+    {
+        Console.WriteLine();
+        Console.WriteLine(
+            "Error: ingrese un precio numérico válido."
+        );
+
+        return;
+    }
+
+    if(precioNoche <= 0)
+    {
+        Console.WriteLine();
+        Console.WriteLine(
+            "Error: el precio debe ser mayor que cero."
+        );
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Seleccione el estado de la habitación:");
+    Console.WriteLine("1. Disponible");
+    Console.WriteLine("2. Ocupada");
+    Console.Write("Opción: ");
+
+    bool estadoConvertido = int.TryParse(
+        Console.ReadLine(),
+        out int opcionEstado
+    );
+
+    if(!estadoConvertido)
+    {
+        Console.WriteLine();
+        Console.WriteLine(
+            "Error: debe ingresar una opción numérica."
+        );
+
+        return;
+    }
+
+    string estadoHabitacion = "";
+
+    switch (opcionEstado)
+    {
+        case 1:
+            estadoHabitacion = "Disponible";
+            break;
+        
+        case 2:
+            estadoHabitacion = "Ocupada";
+            break;
+        
+        default:
+            Console.WriteLine();
+            Console.WriteLine(
+                "Error: seleccione 1 para Disponible o 2 para Ocupada."
+            );
+
+            return;
+    }
+
+    string habitacionRegistrada =
+        $"Habitación {numeroHabitacion} | " +
+        $"Tipo: {tipoHabitacion} | " +
+        $"Precio: S/ {precioNoche:F2} | " +
+        $"Estado: {estadoHabitacion}";
+
+    habitaciones.Add(habitacionRegistrada);
+
+    Console.WriteLine();
+    Console.WriteLine("=== HABITACIÓN REGISTRADA ===");
+    Console.WriteLine(habitacionRegistrada);
+    Console.WriteLine(
+        $"Total registrado: {habitaciones.Count}"
+    );
+}
